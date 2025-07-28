69ff452c3c0c40269757c18a792ed6a2b03becc5
kunaljr <kunalshelke123@gmail.com>
2025-07-28 05:52:48 +0530

    Added redis for rate limiting and session #11.

---
+++ middleware/rateLimiter.js
@@ -0,0 +1,17 @@
+// middleware/rateLimiter.js
+const rateLimit = require('express-rate-limit');
+const {RedisStore} = require('rate-limit-redis');
+const redisClient = require('../config/redis');
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: '🚫 Too many requests, please try again later.',
+  standardHeaders: true,
+  legacyHeaders: false,
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.sendCommand(args),
+  }),
+});
+
+module.exports = limiter;
