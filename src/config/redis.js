71a46578dd3ad25bcdeff30f36842073b80b49b3
kunaljr <kunalshelke123@gmail.com>
2025-07-29 07:55:47 +0530

    Updated changes #13.

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,13 +1,6 @@
 const {createClient} = require('redis');
 
-const client = createClient({
-    username: 'default',
-    password: 'o3ZDlpWAVofE7SUYAp7kJnBBlPTeG0at',
-    socket: {
-        host: 'redis-18999.c14.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: 18999
-    }
-});
+const client = createClient();
 
 client.on('error', (err) => console.error('❌ Redis Client Error', err));
 
